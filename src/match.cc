78d3821221ea3d30eac12307336242f915182da4
JMounier <julien.mounier@epita.fr>
2018-08-22 14:49:33 +0200

    doc: add documentation

--- src/match.cc
+++ src/match.cc
@@ -3,22 +3,22 @@
 #include <algorithm>
 #include <string>
 
-bool done(const graph_ty& g)
-{
-    for (const auto& elm : g)
-        for(auto b : elm)
+// Check every match has been played
+// (the array/graph only contains True)
+bool done(const graph_ty &g) {
+    for (const auto &elm : g)
+        for (auto b : elm)
             if (!b)
                 return false;
     return true;
 }
 
-int count_left(const graph_ty& g, int max)
-{
+// Count the number of match remaining to be played
+int count_left(const graph_ty &g, int max) {
     int res = 0;
-    for (const auto& elm : g)
-        for(auto b : elm)
-            if (b)
-            {
+    for (const auto &elm : g)
+        for (auto b : elm)
+            if (b) {
                 res++;
                 if (res == max)
                     return max;
@@ -26,22 +26,28 @@ int count_left(const graph_ty& g, int max)
     return res;
 }
 
-std::vector<std::tuple<Team*, Team*>> list_remaining(const graph_ty& g, teams_list& teams)
-{
-    std::vector<std::tuple<Team*,Team*>> res;
+// List the remaining matches (order: highest priority first)
+// return a list of tuple (pair)
+// Each tuple represent the two playing teams.
+std::vector <std::tuple<Team *, Team *>> list_remaining(const graph_ty &g, teams_list &teams) {
+    std::vector <std::tuple<Team *, Team *>> res;
 
+    // list every non played match.
     for (auto i = 0; i < g.size(); ++i)
-        for(auto j = 0; j < i; ++j)
+        for (auto j = 0; j < i; ++j)
             if (!g[i][j])
                 res.push_back(std::make_tuple(&teams[j], &teams[i]));
 
+    // Sort the list by priority (least played first, most slept then)
     std::sort(res.begin(), res.end(),
-              [](const std::tuple<Team*,Team*>& a, const std::tuple<Team*,Team*>& b)
-              {
+            // comparison function (returns true if tuple A's priority is superior than tuple B's)
+              [](const std::tuple<Team *, Team *> &a, const std::tuple<Team *, Team *> &b) {
+                  // sum number of match played by both teams until now
                   int sum_a = std::get<0>(a)->count<Team::PLAY>() + std::get<1>(a)->count<Team::PLAY>();
                   int sum_b = std::get<0>(b)->count<Team::PLAY>() + std::get<1>(b)->count<Team::PLAY>();
                   if (sum_a != sum_b)
                       return sum_a < sum_b;
+                  // sum number of match spent sleeping by both teams until now
                   sum_a = std::get<0>(a)->count<Team::SLEEP>() + std::get<1>(a)->count<Team::SLEEP>();
                   sum_b = std::get<0>(b)->count<Team::SLEEP>() + std::get<1>(b)->count<Team::SLEEP>();
                   return sum_a > sum_b;
@@ -49,11 +55,10 @@ std::vector<std::tuple<Team*, Team*>> list_remaining(const graph_ty& g, teams_li
     return res;
 };
 
-graph_ty gen_matches(int nb_team)
-{
+// Create the array/graph of match to play
+graph_ty gen_matches(int nb_team) {
     graph_ty res;
-    for (auto i = 0; i < nb_team; i++)
-    {
+    for (auto i = 0; i < nb_team; i++) {
         res.emplace_back();
         for (auto j = 0; j < nb_team; j++)
             res.back().emplace_back(j == i);
@@ -61,116 +66,160 @@ graph_ty gen_matches(int nb_team)
     return res;
 }
 
-teams_list gen_teams(int nb_team)
-{
+// Create the placeholder for a given number of teams
+teams_list gen_teams(int nb_team) {
     teams_list res;
     for (auto i = 0; i < nb_team; i++)
         res.push_back(Team(i));
     return res;
 }
 
-std::vector<Team*> clone(teams_list& teams)
-{
-  std::vector<Team*> res;
-  for (auto& elm : teams)
-    res.push_back(&elm);
+// Clone a team placeholder (clone will then be ordered)
+std::vector<Team *> clone(teams_list &teams) {
+    std::vector < Team * > res;
+    for (auto &elm : teams)
+        res.push_back(&elm);
 
-  return res;
+    return res;
 }
 
-match_list make_match(int nb_team, int nb_field)
-{
+// Initiate internal data structures and return the generated matches
+match_list make_match(int nb_team, int nb_field) {
+    // init array/graph
     auto graph = gen_matches(nb_team);
 
+    // const teams placeholder (static ordering)
     teams_list teams = gen_teams(nb_team);
-    std::vector<Team*> teams_cln = clone(teams);
+    // clone of 'teams' (will be reordered later)
+    std::vector < Team * > teams_cln = clone(teams);
+    // result (match list)
     match_list res;
 
+    // round counter
     int nb_round = 0;
 
-    while (!done(graph))
-    {
+    while (!done(graph)) {
+        // generate a round
         gen_round(res, graph, teams_cln, teams, nb_field);
+
+        // increment round counter
         nb_round++;
+        // add sleep to teams (keep number of action equal for every team)
         apply_sleep(teams, nb_round);
     }
 
     return res;
 }
 
-void apply_sleep(teams_list& teams, int num)
-{
-    for (auto& elm : teams)
+// Make every team that did not play on rule take turns of sleep.
+void apply_sleep(teams_list &teams, int num) {
+    for (auto &elm : teams)
         while (elm.actions.size() < num)
             elm.push(Team::SLEEP);
 }
 
-void gen_round(match_list& buf, graph_ty& graph, std::vector<Team*>& teams, teams_list& teams_stat, int nb_field)
-{
+// Generate a single round
+// bur : result buffer
+// graph: array/graph of matches
+// teams: team list
+// teams_stat (the fixed ordered list mostly equal to teams)
+// nb_fields: number of field that can be used
+void gen_round(match_list &buf, graph_ty &graph, std::vector<Team *> &teams, teams_list &teams_stat, int nb_field) {
+
+    // TODO: fix the arithmetic
     const int nb_match = graph.size() * (graph.size() - 1) / 2;
+    // number of match to aim for this round
     int max = count_left(graph, nb_field);
 
+    // list every remaining match to play (order: highest priority first)
     auto possible = list_remaining(graph, teams_stat);
 
+    // buffer for currently selected matches for the round
     match_tup selected;
 
+    // Standard algorithm + functor
+    // Simply: sort the list of teams (order: highest judging priority first)
     std::sort(teams.begin(), teams.end(),
-              [](const Team* a, const Team* b)
-              {
+            // Comparison function
+            // return true if A has higher priority than B
+              [](const Team *a, const Team *b) {
+
                   int sum_a = a->count<Team::JUDGE>();
                   int sum_b = b->count<Team::JUDGE>();
+                  // compare number of time spent judging
                   if (sum_a != sum_b)
                       return sum_a < sum_b;
+                  // compare number of time spent sleeping
                   return a->count<Team::SLEEP>() > b->count<Team::SLEEP>();
               });
 
-    for (auto i = 0; i < max; ++i)
-    {
+    // try to find 'max' match to play
+    for (auto i = 0; i < max; ++i) {
         bool found = false;
 
+        // look through every possible match (previously listed)
         for (auto it = possible.begin(); it != possible.end(); ++it)
-            if (compatible(selected, std::get<0>(*it)) && compatible(selected, std::get<1>(*it)))
-            {
+            // check if playing teams are compatible with selected
+            if (compatible(selected, std::get<0>(*it)) && compatible(selected, std::get<1>(*it))) {
+                // choose a judge for the match
                 auto judge = find_judge(selected, *it, teams, nb_match);
+                // if no judge can be found just consider the match as not compatible
                 if (!judge)
+                    // TODO: improvement: maybe list this kind of matches somewhere
+                    //                    cause they are the one than could be switched
                     continue;
                 found = true;
+
+                // select the match (add it to selected)
                 selected.push_back(std::make_tuple<>(judge, std::get<0>(*it), std::get<1>(*it)));
+
+                // remove the selected match from the possible ones (prevent duplicates)
                 possible.erase(possible.begin(), it + 1);
                 break;
             }
 
+        // stop computation if no match found
         if (!found)
             break;
     }
 
+    // check error
     if (selected.size() == 0)
         throw std::runtime_error("cannot find any valid match to play");
 
+    // apply the selection to buf
     apply_selection(buf, graph, selected);
 }
 
-void apply_selection(match_list& buf, graph_ty& graph, match_tup& selected)
-{
+// Add the selected matches as a new round
+// update the array/graph of played matches
+// make playing and judging team take there action
+void apply_selection(match_list &buf, graph_ty &graph, match_tup &selected) {
     int field = 0;
-    for(const auto& elm : selected)
-    {
+    for (const auto &elm : selected) {
+        // extract the ids
         int j = std::get<0>(elm)->id;
         int t1 = std::get<1>(elm)->id;
         int t2 = std::get<2>(elm)->id;
+        // add the match to the results list
         buf.push_back(std::make_tuple<>(field++, j, t1, t2));
 
+        // update array/graph
         graph[t1][t2] = true;
         graph[t2][t1] = true;
+
+        // take action
         std::get<0>(elm)->push(Team::JUDGE);
         std::get<1>(elm)->push(Team::PLAY);
         std::get<2>(elm)->push(Team::PLAY);
     }
 }
 
-Team* find_judge(const match_tup& selected, const std::tuple<Team*, Team*>& match,
-                 std::vector<Team*>& teams, int nb_match)
-{
+// Find a team to judge the match
+// teams is an ordered list (order: highest priority to judge first)
+Team *find_judge(const match_tup &selected, const std::tuple<Team *, Team *> &match,
+                 std::vector<Team *> &teams, int nb_match) {
+    // check the maximum number of time a team can judge
     int max = nb_match / teams.size() + 1;
 
     for (auto elm : teams)
@@ -181,45 +230,45 @@ Team* find_judge(const match_tup& selected, const std::tuple<Team*, Team*>& matc
     return nullptr;
 }
 
-bool compatible(const match_tup& selected,const Team* team)
-{
+// Check if a given team can be used to create a match
+// (team not already playing or judging)
+bool compatible(const match_tup &selected, const Team *team) {
+    // Use a standard algorithm + functor
+    // Simply: Goes through the list and returns if the team is never playing nor judging
     return !std::any_of(selected.cbegin(), selected.cend(),
-                       [&team](const std::tuple<Team*, Team*, Team*> elm)
-                       {
-                           return std::get<0>(elm) == team || std::get<1>(elm) == team || std::get<2>(elm) == team;
-                       });
+                        [&team](const std::tuple<Team *, Team *, Team *> elm) {
+                            return std::get<0>(elm) == team || std::get<1>(elm) == team || std::get<2>(elm) == team;
+                        });
 }
 
-std::ostream& operator<<(std::ostream& out, const match_list& list)
-{
+// Dump the results in human readable way
+std::ostream &operator<<(std::ostream &out, const match_list &list) {
     out << "matches:" << std::endl;
     int last = 1;
     int round = 0;
-    for (auto it = list.cbegin(); it != list.cend(); it++)
-    {
-        if (std::get<0>(*it) <= last)
-        {
+    for (auto it = list.cbegin(); it != list.cend(); it++) {
+        if (std::get<0>(*it) <= last) {
             out << "ROUND N°" << round << std::endl;
             round++;
         }
         last = std::get<0>(*it);
         out << "terrain: " << last;
         out << ",\"arbitre\":" << std::get<1>(*it);
-        out <<",\"equipes\":";
+        out << ",\"equipes\":";
         out << "[" << std::get<2>(*it) << "," << std::get<3>(*it) << "]" << std::endl;
     }
     return out;
 }
-void dump(std::ostream& out, const match_list& list)
-{
+
+// Dump the results in JSON format
+void dump(std::ostream &out, const match_list &list) {
     out << "{\"matches\":[";
-    for (auto it = list.cbegin(); it != list.cend(); it++)
-    {
+    for (auto it = list.cbegin(); it != list.cend(); it++) {
         if (it != list.cbegin())
             out << ',';
         out << "{\"terrain\":" << std::get<0>(*it);
         out << ",\"arbitre\":" << std::get<1>(*it);
-        out <<",\"equipes\":";
+        out << ",\"equipes\":";
         out << "[" << std::get<2>(*it) << "," << std::get<3>(*it) << "]}";
     }
 
--- src/structures.hh
+++ src/structures.hh
@@ -4,7 +4,7 @@
 #include <tuple>
 #include <deque>
 
-using graph_ty = std::vector<std::vector<bool>>;
+using graph_ty = std::vector <std::vector<bool>>;
 
 // tuple<terrain, arbitre, team1, team2>
 using match_ty = std::tuple<int, int, int, int>;
@@ -12,8 +12,10 @@ using match_list = std::vector<match_ty>;
 
 using teams_ty = std::tuple<int, int, int>;
 
-struct Team
-{
+// Class representing a team
+struct Team {
+    // Enum of actions a team can take during one round
+    // Only one action per round
     enum state {
         PLAY,
         SLEEP,
@@ -21,26 +23,30 @@ struct Team
     };
 
     Team(int n)
-            : id{n}
-    {}
+            : id{n} {}
 
+    // The team unique identifier
     int id;
-    std::deque<state> actions;
+    // The list of action taken by the team
+    std::deque <state> actions;
 
+    // Count the number of 'T' action taken by the team
     template<state T>
-    inline int count() const
-    {
+    inline int count() const {
         auto res = 0;
-        for (const auto& elm : actions)
+        for (const auto &elm : actions)
             res += elm == T;
         return res;
     }
 
-    inline void push(state t)
-    {
+    // Add an action to the list
+    inline void push(state t) {
         actions.push_back(t);
     }
 };
 
+// Type of Team placeholder
 using teams_list = std::vector<Team>;
-using match_tup = std::vector<std::tuple<Team*, Team*, Team*>>;
+
+// temporary Type representing a match (when the field is not decided yet)
+using match_tup = std::vector <std::tuple<Team *, Team *, Team *>>;
